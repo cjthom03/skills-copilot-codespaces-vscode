3db4a60ac7b4ba06a94778b938448b6dd8ce30ff
Charlie Thomas <34196444+cjthom03@users.noreply.github.com>
2023-06-27 18:04:02 +0000

    step 4

---
+++ comments.js
@@ -0,0 +1,26 @@
+// Create web server
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(bodyParser.json());
+
+// Data
+const comments = [
+  { id: 1, author: 'Bob', text: 'Awesome!' },
+  { id: 2, author: 'Alice', text: 'Cool!' },
+];
+
+// Routes
+app.get('/comments', (req, res) => {
+  res.send(comments);
+});
+
+app.post('/comments', (req, res) => {
+  const newComment = {
+    id: comments.length + 1,
+
